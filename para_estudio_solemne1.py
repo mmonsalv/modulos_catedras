f35f479216f0ff6944e44190bebebf78dbcd6a5c
Mauricio Monsalve <mauricio.monsalve@gmail.com>
2023-08-31 10:07:38 -0400

    Add files via upload

---
+++ para_estudio_solemne1.py
@@ -0,0 +1,276 @@
+def referencias_apa(num):
+	if num==0:
+		return "Helmbold, R. L. (1993). Osipov: The ‘Russian Lanchester’. European Journal of Operational Research, 65(2), 278-288."
+	elif num==1:
+		return "Homer-Dixon, T. F. (1987). A common misapplication of the Lanchester square law: a research note. ternational Security, 12(1), 135-139."
+	elif num==2:
+		return "Stanescu, M., Barriga, N., & Buro, M. (2015). Using Lanchester attrition laws for combat prediction in StarCraft. In Proceedings of the AAAI Conference on Artificial Intelligence and Interactive Digital Entertainment (Vol. 11, No. 1, pp. 86-92)."
+	elif num==3:
+		return "Artelli, M. J., & Deckro, R. F. (2008). Modeling the Lanchester laws with system dynamics. The Journal of Defense Modeling and Simulation, 5(1), 1-20."
+	elif num==4:
+		return "Lucas, T. W., & Dinges, J. A. (2004). The effect of battle circumstances on fitting Lanchester equations to the Battle of Kursk. Military Operations Research, 17-30."
+	elif num==5:
+		return "Wiper, M. P., Pettit, L. I., & Young, K. D. (2000). Bayesian inference for a Lanchester type combat model. Naval Research Logistics (NRL), 47(7), 541-558."
+	elif num==6:
+		return "Colegrave, R. K., & Hyde, J. M. (1993). The Lanchester square-law model extended to a (2, 2) conflict. IMA Journal of Applied Mathematics, 51(2), 95-109."
+	elif num==7:
+		return "Taylor, J. G. (1973). Target selection in Lanchester combat: Linear‐law attrition process. Naval Research Logistics Quarterly, 20(4), 673-697."
+	else:
+		return None
+
+def referencias_van(num):
+    if num==0:
+        return "Lu GY, Wong DW. An adaptive inverse-distance weighting spatial interpolation technique. Computers & geosciences. 2008 Sep 1;34(9):1044-55."
+    elif num==1:
+        return "Nemes A, Rawls WJ, Pachepsky YA. Use of the nonparametric nearest neighbor approach to estimate soil hydraulic properties. Soil Science Society of America Journal. 2006 Mar;70(2):327-36."
+    elif num==2:
+        return "Haghverdi A, Leib BG, Cornelis WM. A simple nearest-neighbor technique to predict the soil water retention curve. Transactions of the ASABE. 2015;58(3):697-705."
+    elif num==3:
+        return "Journel AG, Rossi ME. When do we need a trend model in kriging?. Mathematical Geology. 1989 Oct;21:715-39."
+    elif num==4:
+        return "Tomczak M. Spatial interpolation and its uncertainty using automated anisotropic inverse distance weighting (IDW)-cross-validation/jackknife approach. Journal of Geographic Information and Decision Analysis. 1998 May;2(2):18-30."
+    elif num==5:
+        return "Liu ZN, Yu XY, Jia LF, Wang YS, Song YC, Meng HD. The influence of distance weight on the inverse distance weighted method for ore-grade estimation. Scientific Reports. 2021 Jan 29;11(1):2689."
+    elif num==6:
+        return "Kleijnen JP. Kriging metamodeling in simulation: A review. European journal of operational research. 2009 Feb 1;192(3):707-16."
+    elif num==7:
+        return "Heung B, Ho HC, Zhang J, Knudby A, Bulmer CE, Schmidt MG. An overview and comparison of machine-learning techniques for classification purposes in digital soil mapping. Geoderma. 2016 Mar 1;265:62-77."
+    elif num==8:
+        return "Wackernagel H, Wackernagel H. Ordinary kriging. Multivariate geostatistics: an introduction with applications. 2003:79-88."
+    elif num==9:
+        return "De Mesnard L. Pollution models and inverse distance weighting: Some critical remarks. Computers & Geosciences. 2013 Mar 1;52:459-69."
+    elif num==10:
+        return "Zidan K, Sbahi S, Hejjaj A, Ouazzani N, Assabbane A, Mandi L. Removal of bacterial indicators in on-site two-stage multi-soil-layering plant under arid climate (Morocco): prediction of total coliform content using K-nearest neighbor algorithm. Environmental Science and Pollution Research. 2022 Oct;29(50):75716-29."
+    elif num==11:
+        return "Antropov O, Rauste Y, Astola H, Praks J, Häme T, Hallikainen MT. Land cover and soil type mapping from spaceborne PolSAR data at L-band with probabilistic neural network. IEEE Transactions on Geoscience and Remote Sensing. 2013 Dec 5;52(9):5256-70."
+    elif num==12:
+        return "Yamamoto JK. An alternative measure of the reliability of ordinary kriging estimates. Mathematical Geology. 2000 May;32:489-509."
+    elif num==13:
+        return "Beucher A, Rasmussen CB, Moeslund TB, Greve MH. Interpretation of convolutional neural networks for acid sulfate soil classification. Frontiers in Environmental Science. 2022 Jan 19;9:679."
+    elif num==14:
+        return "Dimitrakopoulos R, Luo X. Spatiotemporal modelling: covariances and ordinary kriging systems. In Geostatistics for the Next Century: An International Forum in Honour of Michel David’s Contribution to Geostatistics, Montreal, 1993 1994 (pp. 88-93). Dordrecht: Springer Netherlands."
+    elif num==15:
+        return "Pasolli E, Melgani F, Tuia D, Pacifici F, Emery WJ. SVM active learning approach for image classification using spatial information. IEEE Transactions on Geoscience and Remote Sensing. 2013 Jun 13;52(4):2217-33."
+    elif num==16:
+        return "Pandith V, Kour H, Singh S, Manhas J, Sharma V. Performance evaluation of machine learning techniques for mustard crop yield prediction from soil analysis. Journal of scientific research. 2020;64(2):394-8."
+    elif num==17:
+        return "Matteoli S, Veracini T, Diani M, Corsini G. Models and methods for automated background density estimation in hyperspectral anomaly detection. IEEE transactions on geoscience and remote sensing. 2012 Oct 8;51(5):2837-52."
+    elif num==18:
+        return "Bartier PM, Keller CP. Multivariate interpolation to incorporate thematic surface data using inverse distance weighting (IDW). Computers & Geosciences. 1996 Aug 1;22(7):795-9."
+    elif num==19:
+        return "Mansuy N, Thiffault E, Paré D, Bernier P, Guindon L, Villemaire P, Poirier V, Beaudoin A. Digital mapping of soil properties in Canadian managed forests at 250 m of resolution using the k-nearest neighbor method. Geoderma. 2014 Dec 1;235:59-73."
+    elif num==20:
+        return "Lark RM, Webster R. Analysis and elucidation of soil variation using wavelets. European Journal of Soil Science. 1999 Jul;50(2):185-206."
+    else:
+        return None
+
+def referencias_titulos(num):
+	if num==0:
+		return "Microcomputer videogame based training"
+	elif num==1:
+		return "Are videogame training gains specific or general?"
+	elif num==2:
+		return "Videogame addiction and its treatment"
+	elif num==3:
+		return "Supercade: A visual history of the videogame age 1971-1984"
+	elif num==4:
+		return "Videogame addiction: Further thoughts and observations"
+	elif num==5:
+		return "Videogame reward types"
+	elif num==6:
+		return "Game-based learning: The impact of flow state and videogame self-efficacy"
+	elif num==7:
+		return "Learning and videogames: An unexploited synergy"
+	elif num==8:
+		return "Educational videogame design"
+	elif num==9:
+		return "Videogame addiction: Further thoughts and observations"
+	elif num==10:
+		return "Educational video game design: A review of the literature"
+	elif num==11:
+		return "Design of educational multiplayer videogames: A vision from collaborative learning"
+	elif num==12:
+		return "Transformational play as a curricular scaffold: Using videogames to support science education"
+	elif num==13:
+		return "Video game addiction in children and teenagers in Taiwan"
+	elif num==14:
+		return "Educational video game design: A review of the literature"
+	elif num==15:
+		return "Does video game addiction exist"
+	elif num==16:
+		return "Play along-an approach to videogame music"
+	elif num==17:
+		return "Video game addiction and social responsibility"
+	elif num==18:
+		return "Case study: Film music vs. video-game music: The case of Silent Hill"
+	else:
+		return None
+
+def nombres1(num):
+    if num==0:
+        return "Yelena"
+    elif num==1:
+        return "Alexei"
+    elif num==2:
+        return "Leopold"
+    elif num==3:
+        return "Vlad"
+    elif num==4:
+        return "Alexei"
+    elif num==5:
+        return "Heindrich"
+    elif num==6:
+        return "Francesca"
+    elif num==7:
+        return "Kim"
+    elif num==8:
+        return "Jan"
+    elif num==9:
+        return "Vlad"
+    elif num==10:
+        return "Andrei"
+    elif num==11:
+        return "Ben"
+    elif num==12:
+        return "Alexei"
+    elif num==13:
+        return "Kim"
+    elif num==14:
+        return "Jan"
+    elif num==15:
+        return "Boris"
+    elif num==16:
+        return "Leopold"
+    elif num==17:
+        return "Katariina"
+    elif num==18:
+        return "Anibald"
+    elif num==19:
+        return "Alexei"
+    elif num==20:
+        return "Yelena"
+    elif num==21:
+        return "Francesca"
+    elif num==22:
+        return "Ben"
+    elif num==23:
+        return "Andrei"
+    elif num==24:
+        return "Nikolai"
+    elif num==25:
+        return "Francesca"
+    elif num==26:
+        return "Nikolai"
+    elif num==27:
+        return "Hans"
+    elif num==28:
+        return "Olga"
+    elif num==29:
+        return "Jan"
+    else:
+        return None
+
+def nombres2(num):
+    if num==0:
+        return "Clark"
+    elif num==1:
+        return "Sergei"
+    elif num==2:
+        return "Katariina"
+    elif num==3:
+        return "Yelena"
+    elif num==4:
+        return "William"
+    elif num==5:
+        return "Otto"
+    elif num==6:
+        return "Sacha"
+    elif num==7:
+        return "Nadia"
+    elif num==8:
+        return "Sabrina"
+    elif num==9:
+        return "Andrei"
+    elif num==10:
+        return "Nikolai"
+    elif num==11:
+        return "Kim"
+    elif num==12:
+        return "Andrei"
+    elif num==13:
+        return "Sabrina"
+    elif num==14:
+        return "Anibald"
+    elif num==15:
+        return "Vlad"
+    elif num==16:
+        return "Alessio"
+    elif num==17:
+        return "Marcy"
+    elif num==18:
+        return "Summer"
+    elif num==19:
+        return "Hans"
+    elif num==20:
+        return "Beatrice"
+    elif num==21:
+        return "Hans"
+    elif num==22:
+        return "Leopold"
+    elif num==23:
+        return "Hannah"
+    elif num==24:
+        return "Hannah"
+    elif num==25:
+        return "Hannah"
+    elif num==26:
+        return "Laura"
+    elif num==27:
+        return "Ulrich"
+    elif num==28:
+        return "Alessio"
+    elif num==29:
+        return "Joanna"
+    elif num==30:
+        return "Beatrice"
+    elif num==31:
+        return "Clark"
+    elif num==32:
+        return "Betty"
+    elif num==33:
+        return "Ferdinand"
+    elif num==34:
+        return "Vlad"
+    elif num==35:
+        return "Clark"
+    elif num==36:
+        return "Beatrice"
+    elif num==37:
+        return "Ulrich"
+    elif num==38:
+        return "Anibald"
+    elif num==39:
+        return "Kim"
+    elif num==40:
+        return "Rose"
+    elif num==41:
+        return "Betty"
+    elif num==42:
+        return "Hannah"
+    elif num==43:
+        return "Betty"
+    elif num==44:
+        return "Hans"
+    elif num==45:
+        return "Olga"
+    elif num==46:
+        return "Heindrich"
+    elif num==47:
+        return "Joanna"
+    elif num==48:
+        return "Hans"
+    elif num==49:
+        return "Sabine"
+    else:
+        return None
+
